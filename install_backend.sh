026c6484e1e08c841aeccd0f3cda738ba50d7b18
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-12-20 00:00:21 +0530

    expense tf msg

--- install_backend.sh
+++ install_backend.sh
@@ -10,7 +10,19 @@ cd /app
 unzip /tmp/backend.zip
 npm install
 
-cp /root/4.0.expense-tf-ec2-instances/backend.service /etc/systemd/system/backend.service
+#cp /root/4.0.expense-tf-ec2-instances/backend.service /etc/systemd/system/backend.service
+
+echo "[Unit]
+Description = Backend Service
+
+[Service]
+User=expense
+Environment=DB_HOST="mysql.lingaiah.online"
+ExecStart=/bin/node /app/index.js
+SyslogIdentifier=backend
+
+[Install]
+WantedBy=multi-user.target" >> /etc/systemd/system/backend.service
 
 systemctl daemon-reload
 systemctl start backend
--- install_frontend.sh
+++ install_frontend.sh
@@ -7,6 +7,17 @@ curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expe
 cd /usr/share/nginx/html
 
 unzip /tmp/frontend.zip
-cp /home/ec2-user/4.0.expense-tf-ec2-instances/expense.conf /etc/nginx/default.d/expense.conf
+#cp /home/ec2-user/4.0.expense-tf-ec2-instances/expense.conf /etc/nginx/default.d/expense.conf
+
+
+echo "proxy_http_version 1.1;
+
+location /api/ { proxy_pass http://backend.lingaiah.online:8080/; }
+
+location /health {
+  stub_status on;
+  access_log off;
+}" >> /etc/nginx/default.d/expense.conf
+
 systemctl restart nginx
 
