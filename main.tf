85a965b0b13359f142f95b901c890e34cbd76be7
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-12-21 14:19:09 +0530

    expense ansible roles tf

--- main.tf
+++ main.tf
@@ -1,56 +1,80 @@
-resource "aws_instance" "expense_frontend" {
+
+resource "aws_instance" "roboshop" {
+    for_each = var.instances
     ami           = data.aws_ami.rhel_info.id
-    instance_type = var.frontend_instance.instance_type
+    instance_type = each.value
     vpc_security_group_ids = [var.allow_all]
-    user_data = file("${path.module}/install_frontend.sh")
+    user_data = file("${path.module}/install_{{each.key}}.sh")
 
     tags = {
-        Name = "frontend"
+        Name = each.key
     }
 }
-resource "aws_route53_record" "frontend_r53" {
+resource "aws_route53_record" "roboshop_r53" {
+    for_each = aws_instance.roboshop
     zone_id = var.zone_id
-    name    = "frontend.${var.domain_name}"
+    name    = each.key == "frontend"? var.domain_name : "${each.key}.${var.domain_name}" 
     type    = "A"
     ttl     = 1
-    records = [aws_instance.expense_frontend.public_ip]
+    records = each.key == "frontend" ? [each.value.public_ip]:[each.value.private_ip]
     allow_overwrite = true
 }
 
-resource "aws_instance" "expense_backend" {
-    ami           = data.aws_ami.rhel_info.id
-    instance_type = var.backend_instance.instance_type
-    vpc_security_group_ids = [var.allow_all]
-    user_data = file("${path.module}/install_backend.sh")
 
-    tags = {
-        Name = "backend"
-    }
-}
-resource "aws_route53_record" "backend_r53" {
-    zone_id = var.zone_id
-    name    = "backend.${var.domain_name}"
-    type    = "A"
-    ttl     = 1
-    records = [aws_instance.expense_backend.public_ip]
-    allow_overwrite = true
-}
 
-resource "aws_instance" "expense_mysql" {
-    ami           = data.aws_ami.rhel_info.id
-    instance_type = var.mysql_instance.instance_type
-    vpc_security_group_ids = [var.allow_all]
-    user_data = file("${path.module}/install_mysql.sh")
+# resource "aws_instance" "expense_frontend" {
+#     ami           = data.aws_ami.rhel_info.id
+#     instance_type = var.frontend_instance.instance_type
+#     vpc_security_group_ids = [var.allow_all]
+#     user_data = file("${path.module}/install_frontend.sh")
 
-    tags = {
-        Name = "mysql"
-    }
-}
-resource "aws_route53_record" "mysql_r53" {
-    zone_id = var.zone_id
-    name    = "mysql.${var.domain_name}"
-    type    = "A"
-    ttl     = 1
-    records = [aws_instance.expense_mysql.public_ip]
-    allow_overwrite = true
-}
+#     tags = {
+#         Name = "frontend"
+#     }
+# }
+# resource "aws_route53_record" "frontend_r53" {
+#     zone_id = var.zone_id
+#     name    = "frontend.${var.domain_name}"
+#     type    = "A"
+#     ttl     = 1
+#     records = [aws_instance.expense_frontend.public_ip]
+#     allow_overwrite = true
+# }
+
+# resource "aws_instance" "expense_backend" {
+#     ami           = data.aws_ami.rhel_info.id
+#     instance_type = var.backend_instance.instance_type
+#     vpc_security_group_ids = [var.allow_all]
+#     user_data = file("${path.module}/install_backend.sh")
+
+#     tags = {
+#         Name = "backend"
+#     }
+# }
+# resource "aws_route53_record" "backend_r53" {
+#     zone_id = var.zone_id
+#     name    = "backend.${var.domain_name}"
+#     type    = "A"
+#     ttl     = 1
+#     records = [aws_instance.expense_backend.public_ip]
+#     allow_overwrite = true
+# }
+
+# resource "aws_instance" "expense_mysql" {
+#     ami           = data.aws_ami.rhel_info.id
+#     instance_type = var.mysql_instance.instance_type
+#     vpc_security_group_ids = [var.allow_all]
+#     user_data = file("${path.module}/install_mysql.sh")
+
+#     tags = {
+#         Name = "mysql"
+#     }
+# }
+# resource "aws_route53_record" "mysql_r53" {
+#     zone_id = var.zone_id
+#     name    = "mysql.${var.domain_name}"
+#     type    = "A"
+#     ttl     = 1
+#     records = [aws_instance.expense_mysql.public_ip]
+#     allow_overwrite = true
+# }
--- variables.tf
+++ variables.tf
@@ -3,24 +3,33 @@ variable "allow_all" {
    default = "sg-06b1b57b365846051"
 }
 variable "zone_id" {
-    default = "Z05050032GOS7XGPQ48FJ"
+    default = "Z0660538W3DX3HZU59ZR"
 }
 variable "domain_name" {
   default = "lingaiah.online"
 }
 
-variable "frontend_instance" {
-   default = {
-        instance_type  = "t2.micro"
-   }
-}
-variable "backend_instance" {
-   default = {
-        instance_type  = "t2.micro"
-   }
-}
-variable "mysql_instance" {
-   default = {
-        instance_type  = "t2.small"
-   }
+# variable "frontend_instance" {
+#    default = {
+#         instance_type  = "t2.micro"
+#    }
+# }
+# variable "backend_instance" {
+#    default = {
+#         instance_type  = "t2.micro"
+#    }
+# }
+# variable "mysql_instance" {
+#    default = {
+#         instance_type  = "t2.small"
+#    }
+# }
+
+variable "instances" {
+    type     = map
+    default = {
+        frontend  = "t2.micro"
+        backend   = "t2.micro"
+        mysql     = "t3.small"
+    }
 }
